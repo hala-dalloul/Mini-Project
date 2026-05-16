bb7b5dfa6f37b8322a33d3c2b6713e422686d3b3
Hala M. Dalloul <haladalloul42@gmail.com>
2026-05-16 23:37:19 +0300

    Update `main` function to successful running

--- bin/mini_project.dart
+++ bin/mini_project.dart
@@ -7,5 +7,24 @@ void main(List<String> arguments) {
 
   String? name = stdin.readLineSync();
 
-  print(name);
+  stdout.write("Enter number of marks: ");
+  int? numberOfMarks = int.tryParse(stdin.readLineSync()?? '');
+  List<int> marks = [];
+
+  if(numberOfMarks! > 0){
+    for(int i = 0; i< numberOfMarks; i++){
+      stdout.write("Enter mark number ${i + 1}: ");
+      int? mark = int.tryParse(stdin.readLineSync()?? '');
+      if(mark != null && mark > 0 && mark < 100){
+        marks.add(mark);
+      }
+    }
+  }
+
+
+  if(marks.isNotEmpty) {
+    print("******* Hello $name*******\n**Your Average is: ${mini_project.calculateTheAverage(marks)}**");
+  } else {
+    print(" Hello $name,\nYour list of marks is null");
+  }
 }
