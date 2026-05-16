59a53f57bbba9789bd117972eb26991c2ce25154
Hala M. Dalloul <haladalloul42@gmail.com>
2026-05-16 20:15:55 +0300

    Add project structure

---
+++ bin/mini_project.dart
@@ -0,0 +1,5 @@
+import 'package:mini_project/mini_project.dart' as mini_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mini_project.calculate()}!');
+}
---
+++ test/mini_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:mini_project/mini_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
